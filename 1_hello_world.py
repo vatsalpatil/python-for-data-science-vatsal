670ad0461ec34c6293d79632d399a154f5b5bc22
vatsalpatil <84773161+vatsalpatil@users.noreply.github.com>
2021-07-25 13:51:13 +0530

    Add files via upload

---
+++ 1_hello_world.py
@@ -0,0 +1,18 @@
+
+print("hello world")
+
+nums =[]
+
+# This is a append list
+nums.append("75")
+nums.append("vatsal")
+nums.append("p")
+
+print (nums)
+
+feedback = input("Do you like it? (y/n)")
+if(feedback == "y"):print("thanks bro")
+else:print("Sorry")
+
+
+
